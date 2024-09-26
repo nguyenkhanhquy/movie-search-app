058cd8ff08640b186c487895665d46623e3f5f12
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-26 22:55:45 +0700

    update: App.js

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { ExpoRoot } from "expo-router";
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import { UserProvider } from "./context/UserContext";
+import { ThemeProvider } from "react-native-paper";
 
 export default function App() {
     const ctx = require.context("./app/_tabs");
     return (
-        <UserProvider>
-            <ExpoRoot context={ctx} />
-        </UserProvider>
+        <ThemeProvider>
+            <UserProvider>
+                <ExpoRoot context={ctx} />
+            </UserProvider>
+        </ThemeProvider>
     );
 }
 
